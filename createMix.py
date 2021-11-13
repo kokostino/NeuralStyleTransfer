b59d7267c181486cd455938bbe4d17356d0e3201
Kokostino <56522863+Kokostino@users.noreply.github.com>
2021-11-13 16:18:40 +0100

    new

---
+++ createMix.py
@@ -0,0 +1,158 @@
+# -*- coding: utf-8 -*-
+import os
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import imshow
+from PIL import Image
+import numpy as np
+import tensorflow as tf
+import pprint
+
+content_img=str(input("Enter content image name : "))
+style_img=str(input("Enter style image name: "))
+
+
+pp = pprint.PrettyPrinter(indent=4)
+img_size = 400
+vgg = tf.keras.applications.VGG19(include_top=False,
+                                  input_shape=(img_size, img_size, 3),
+                                  weights='pretrained-model/vgg19_weights_tf_dim_ordering_tf_kernels_notop.h5')
+
+vgg.trainable = False
+pp.pprint(vgg)
+
+def compute_content_cost(content_output, generated_output):
+    a_C = content_output[-1]
+    a_G = generated_output[-1]    
+    m, n_H, n_W, n_C = a_G.get_shape().as_list()    
+    a_C_unrolled = tf.reshape(a_C, shape=[m, n_H * n_W, n_C])
+    a_G_unrolled = tf.reshape(a_G, shape=[m, n_H * n_W, n_C])   
+    J_content = tf.reduce_sum(tf.square(tf.subtract(a_C_unrolled,a_G_unrolled)))/(4* n_H*n_W*n_C)
+    
+    return J_content
+
+
+def gram_matrix(A):
+    GA = tf.matmul(A,tf.transpose(A))
+    
+    return GA
+
+
+def compute_layer_style_cost(a_S, a_G):
+    _, n_H, n_W, n_C = a_G.get_shape().as_list()    
+    a_S = tf.transpose(tf.reshape(a_S, shape=[-1, n_C]))
+    a_G = tf.transpose(tf.reshape(a_G, shape=[-1, n_C]))
+    GS = gram_matrix(a_S)
+    GG = gram_matrix(a_G)
+    J_style_layer = tf.reduce_sum(tf.square(GS - GG))/(4.0 *(( n_H * n_W * n_C)**2))
+    
+    return J_style_layer
+
+
+STYLE_LAYERS = [
+    ('block1_conv1', 0.2),
+    ('block2_conv1', 0.2),
+    ('block3_conv1', 0.2),
+    ('block4_conv1', 0.2),
+    ('block5_conv1', 0.2)]
+
+
+def compute_style_cost(style_image_output, generated_image_output, STYLE_LAYERS=STYLE_LAYERS):
+
+    J_style = 0
+    a_S = style_image_output[:-1]
+    a_G = generated_image_output[:-1]
+    for i, weight in zip(range(len(a_S)), STYLE_LAYERS):  
+        J_style_layer = compute_layer_style_cost(a_S[i], a_G[i])
+        J_style += weight[1] * J_style_layer
+
+    return J_style
+                            
+@tf.function()                            
+def total_cost(J_content, J_style, alpha = 10, beta = 40):
+    J = alpha*J_content+beta*J_style
+    return J
+
+content_image = np.array(Image.open(content_img).resize((img_size, img_size)))
+content_image = tf.constant(np.reshape(content_image, ((1,) + content_image.shape)))
+style_image =  np.array(Image.open(style_img).resize((img_size, img_size)))
+style_image = tf.constant(np.reshape(style_image, ((1,) + style_image.shape)))
+generated_image = tf.Variable(tf.image.convert_image_dtype(content_image, tf.float32))
+noise = tf.random.uniform(tf.shape(generated_image), -0.25, 0.25)
+generated_image = tf.add(generated_image, noise)
+generated_image = tf.clip_by_value(generated_image, clip_value_min=0.0, clip_value_max=1.0)
+
+def get_layer_outputs(vgg, layer_names):
+    """ Creates a vgg model that returns a list of intermediate output values."""
+    outputs = [vgg.get_layer(layer[0]).output for layer in layer_names]
+    model = tf.keras.Model([vgg.input], outputs)
+    return model
+
+content_layer = [('block5_conv4', 1)]
+vgg_model_outputs = get_layer_outputs(vgg, STYLE_LAYERS + content_layer)
+content_target = vgg_model_outputs(content_image)  # Content encoder
+style_targets = vgg_model_outputs(style_image)     # Style enconder
+# Assign the content image to be the input of the VGG model.  
+# Set a_C to be the hidden layer activation from the layer we have selected
+preprocessed_content =  tf.Variable(tf.image.convert_image_dtype(content_image, tf.float32))
+a_C = vgg_model_outputs(preprocessed_content)
+# Assign the input of the model to be the "style" image 
+preprocessed_style =  tf.Variable(tf.image.convert_image_dtype(style_image, tf.float32))
+a_S = vgg_model_outputs(preprocessed_style)
+
+def clip_0_1(image):
+    return tf.clip_by_value(image, clip_value_min=0.0, clip_value_max=1.0)
+
+def tensor_to_image(tensor):
+    tensor = tensor * 255
+    tensor = np.array(tensor, dtype=np.uint8)
+    if np.ndim(tensor) > 3:
+        assert tensor.shape[0] == 1
+        tensor = tensor[0]
+    return Image.fromarray(tensor)
+
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
+
+@tf.function()
+def train_step(generated_image, alpha = 10, beta = 40):
+    with tf.GradientTape() as tape:
+        a_G = vgg_model_outputs(generated_image)
+
+        J_style = compute_style_cost(a_S, a_G)
+        J_content = compute_content_cost(a_C, a_G)
+        J = total_cost(J_content, J_style, alpha, beta)
+    grad = tape.gradient(J, generated_image)
+    optimizer.apply_gradients([(grad, generated_image)])
+    generated_image.assign(clip_0_1(generated_image))
+    return J                          
+
+
+generated_image = tf.Variable(generated_image)
+
+epochs = 2501
+for i in range(epochs):
+    train_step(generated_image)
+    if i % 10 == 0:
+        print(f"Epoch {i} ")
+    if i % 500 == 0:
+        image = tensor_to_image(generated_image)
+        imshow(image)
+        plt.axis("off")
+        #image.save(f"images/image_{i}.jpg")
+        plt.savefig(content_img.split(".")[0]+"PLUS"+style_img.rsplit("/")[1],dpi=400,bbox_inches=None)
+        plt.show() 
+        
+# Show the 3 images in a row
+fig = plt.figure(figsize=(16, 4))
+ax = fig.add_subplot(1, 3, 1)
+imshow(content_image[0])
+plt.axis("off")
+ax.title.set_text('Content image')
+ax = fig.add_subplot(1, 3, 2)
+imshow(style_image[0])
+plt.axis("off")
+ax.title.set_text('Style image')
+ax = fig.add_subplot(1, 3, 3)
+imshow(generated_image[0])
+ax.title.set_text('Generated image')
+plt.axis("off")
+plt.show()
